bf28d633c521935dd81de9e0a97289ef3f232373
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-01-13 17:56:40 -0800

    feat: work in progress on application/facility creation

--- extract/extract.py
+++ extract/extract.py
@@ -1,7 +1,15 @@
-import xlrd
-import psycopg2
+# from smart_open import open as smart_open
+# import builtins
+# import io
+
+# builtins.open = smart_open
+# io.open = smart_open
+
+import builtins
 import os
+import xlrd
 import argparse
+from google.cloud import storage
 import extract_application
 import extract_operator
 import extract_facility
@@ -11,59 +19,92 @@ import extract_equipment
 import extract_energy
 import extract_fuel
 import extract_emission
+from .create_reporting_year import create_2018_reporting_year
 
-def extract_book(book_path, cursor):
-    try:
-        ciip_book = xlrd.open_workbook(book_path)
-    except xlrd.biffh.XLRDError:
-        print('skipping file ' + book_path)
+class MockCursor:
+    def __init__(self):
         return
 
-    application_id = extract_application.extract(ciip_book, cursor, book_path)
+    def execute(self, statement, format_args=()):
+        print(statement % format_args)
+
+    def close(self):
+        return
+
+# The env variable GOOGLE_APPLICATION_CREDENTIALS needs to point at a json file with the gcs credentials
+# "gs://ciip-2018/CIIP applications_2018/CIIP data_final"
+def list_files_in_bucket(bucket_name, prefix):
+    storage_client = storage.Client()
+    def generate_gcs_url(blob): return f'gs://{bucket_name}/{blob.name}'
+    return list(map(generate_gcs_url, storage_client.list_blobs(bucket_name, prefix=prefix)))
+
+def list_blobs_in_bucket(bucket_name, prefix):
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket_name, prefix=prefix)
+    return list(blobs)
+
+def extract_book(blob, cursor):
+    if not os.path.exists('./tmp'):
+        os.makedirs('./tmp')
+    fileName = './tmp/' + blob.name.replace("/", "_")
+    blob.download_to_filename(fileName)
+
+    try:
+        ciip_book = xlrd.open_workbook(fileName)
+    except xlrd.biffh.XLRDError:
+        print('skipping file ' + blob.name)
+        return
+
+    application_id = extract_application.extract(ciip_book, cursor, fileName)
     operator = extract_operator.extract(ciip_book, cursor, application_id)
-    facility = extract_facility.extract(ciip_book, cursor, application_id, operator)
-    extract_contact_info.extract(ciip_book, cursor, application_id, operator['id'], facility['id'])
-    extract_fuel.extract(ciip_book, cursor, application_id, operator['id'], facility['id'])
-    extract_energy.extract(ciip_book, cursor, application_id, operator['id'], facility['id'])
-    extract_production.extract(ciip_book, cursor, application_id, operator['id'], facility['id'])
-    extract_equipment.extract(ciip_book, cursor, application_id, operator['id'], facility['id'])
-    extract_emission.extract(ciip_book, cursor, application_id, operator['id'], facility['id'])
+    facility = extract_facility.extract(
+        ciip_book, cursor, application_id, operator)
+    extract_contact_info.extract(
+        ciip_book, cursor, application_id, operator['id'], facility['id'])
+    extract_fuel.extract(ciip_book, cursor, application_id,
+                            operator['id'], facility['id'])
+    extract_energy.extract(
+        ciip_book, cursor, application_id, operator['id'], facility['id'])
+    extract_production.extract(
+        ciip_book, cursor, application_id, operator['id'], facility['id'])
+    extract_equipment.extract(
+        ciip_book, cursor, application_id, operator['id'], facility['id'])
+    extract_emission.extract(
+        ciip_book, cursor, application_id, operator['id'], facility['id'])
 
     return
 
 
-parser = argparse.ArgumentParser(description='Extracts data from CIIP excel application files and writes it to database')
-parser.add_argument('--dirslist', type=open)
-parser.add_argument('--dir')
-parser.add_argument('--db', default='ggircs_dev')
-parser.add_argument('--host', default='localhost')
-parser.add_argument('--user')
-parser.add_argument('--password')
+parser = argparse.ArgumentParser(
+    description='Extracts data from CIIP excel application files and writes it to database')
+parser.add_argument('--bucket', default='ciip-2018')
+parser.add_argument(
+    '--prefix', default='CIIP applications_2018/CIIP data_final')
+# parser.add_argument('--db', default='ggircs_dev')
+# parser.add_argument('--host', default='localhost')
+# parser.add_argument('--user')
+# parser.add_argument('--password')
 args = parser.parse_args()
 
-conn = psycopg2.connect(dbname=args.db, host=args.host, user=args.user, password=args.password)
-cur = conn.cursor()
+# conn = psycopg2.connect(dbname=args.db, host=args.host, user=args.user, password=args.password)
+# cur = conn.cursor()
 
-directories = []
-if args.dirslist is not None:
-    directories = args.dirslist.read().split('\n')
-else:
-    directories = [args.dir]
+gcs_blobs = list_blobs_in_bucket(args.bucket, args.prefix)[:2]
 
-try:
+cur = MockCursor()
+
+try:    
     cur.execute("select swrs_transform.clone_schema('ciip_2018', 'ciip_2018_load', false);")
-    for directory in directories:
-        if directory.strip() == '':
-            continue
-        for filename in os.listdir(directory):
-            print('parsing: ' + filename)
-            extract_book(os.path.join(directory,filename), cur)
+    create_2018_reporting_year(cur)
+    for blob in gcs_blobs:
+        print('parsing: ' + blob.name)
+        extract_book(blob, cur)
     cur.execute('drop schema ciip_2018 cascade;')
     cur.execute('alter schema ciip_2018_load rename to ciip_2018;')
-    conn.commit()
+    # conn.commit()
 except Exception as e:
-    conn.rollback()
+    # conn.rollback()
     raise e
 finally:
     cur.close()
-    conn.close()
+    # conn.close()
