f79b701c70aeb352c6d28a1178f753deb024168a
Dylan Leard <dylan@button.is>
2021-01-15 16:04:30 -0800

    feat: add reconcile faciliy/operator methods

--- extract/insert_2018_data.py
+++ extract/insert_2018_data.py
@@ -36,14 +36,35 @@ def modify_triggers(cursor, action):
 def validate_schema(form_result):
     # validate form_result data with form_json schema
 
-def reconcile_operator(operator, application):
+def reconcile_operator(operator):
     # Get id of operator in CIIP db || create operator in CIIP db
     # CIIP db id & all operator info
+    cursor.execute(
+        '''
+        select id from ggircs_portal.organisation where swrs_organisation_id = %d;
+        ''',
+        (operator['swrs_operator_id'])
+    )
+    res = cursor.fetchone()
+    operator['ciip_db_id'] = res[0]
+
 
 def reconcile_facility(operator, facility):
     # Get id of facility in CIIP db || create facility in CIIP db
     # Check that the organisation_id in CIIP db = id from reconcile_operator
     # CIIP db id & all fac info
+
+    cursor.execute(
+        '''
+        select id, organisation_id from ggircs_portal.facility where swrs_facility_id = %d;
+        ''',
+        (facility['swrs_facility_id'])
+    )
+    res = cursor.fetchone()
+    if operator.ciip_db_id != res[1]:
+        raise exception('Operator ID mismatch. swrs_facility_id: {res[0]}')
+    else:
+        facility['ciip_db_id'] = res[0]
 
 def create_application(facility, application):
     # Fully manual, create: application, application_revision, application_revision_status='approved', form_result, form_result_status='approved' X ?
