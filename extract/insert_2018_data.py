200e3833656f3c042277c90f8ef9ea6e072b8069
Dylan Leard <dylan@button.is>
2021-01-18 09:33:31 -0800

    refactor: fix syntax errors, update logic in insert method

--- extract/insert_2018_data.py
+++ extract/insert_2018_data.py
@@ -2,12 +2,11 @@ from model.facility import Facility
 import util
 from util import get_sheet_value, none_if_not_number
 
-
 def modify_triggers(cursor, action):
     # disable CIIP db triggers
     cursor.execute(
         '''
-        $do$
+        do $$
           declare table_name text;
           declare table_trigger text;
           declare alter_statement text;
@@ -23,15 +22,11 @@ def modify_triggers(cursor, action):
               alter_statement:=concat('alter table ggircs_portal.', table_name, ' ', %s, ' trigger ', table_trigger);
               execute alter_statement;
             end loop;
-          end;
-
-        $do$
-        select distinct swrs_facility_id from swrs.identifier
-        where identifier_value = %s
+          end
+        $$;
         ''',
         (action)
     )
-
 
 def validate_schema(form_result):
     # validate form_result data with form_json schema
@@ -46,8 +41,10 @@ def reconcile_operator(operator):
         (operator['swrs_operator_id'])
     )
     res = cursor.fetchone()
-    operator['ciip_db_id'] = res[0]
-
+    if res in not None:
+        operator['ciip_db_id'] = res[0]
+    else:
+        # create organisation
 
 def reconcile_facility(operator, facility):
     # Get id of facility in CIIP db || create facility in CIIP db
@@ -61,10 +58,13 @@ def reconcile_facility(operator, facility):
         (facility['swrs_facility_id'])
     )
     res = cursor.fetchone()
-    if operator.ciip_db_id != res[1]:
-        raise exception('Operator ID mismatch. swrs_facility_id: {res[0]}')
+    if res is not None:
+        if operator.ciip_db_id != res[1]:
+            raise exception('Operator ID mismatch. swrs_facility_id: {res[0]}')
+        else:
+            facility['ciip_db_id'] = res[0]
     else:
-        facility['ciip_db_id'] = res[0]
+        # create facility
 
 def create_application(facility, application):
     # Fully manual, create: application, application_revision, application_revision_status='approved', form_result, form_result_status='approved' X ?
@@ -79,8 +79,6 @@ def populate_form_results(application, facility, operator, contact, fuel, emissi
     # insert into ggircs_portal.form_result(fuel info: fuel)
     # insert into ggircs_portal.form_result(emission info: emission)
     # insert into ggircs_portal.form_result(prod info: production, energy)
-    # equipment?
-
 
 def insert_data(cursor, operator, facility, application, contact, fuel, emission, production, energy, equipment):
     modify_triggers('disable')
