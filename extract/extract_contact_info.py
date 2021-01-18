2152e7400ba02170f75186df29f3c6bafa33f55a
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-01-18 13:26:19 -0800

    feat: building administration form_result from extracted data

--- extract/extract_contact_info.py
+++ extract/extract_contact_info.py
@@ -17,31 +17,31 @@ def extract(ciip_book):
     co_header_idx = search_row_index(cert_sheet, 1, 'Signature of Certifying Official')
 
     rep_addr =  {
-        'street_address'  : get_sheet_value(admin_sheet, 24, 1),
+        'streetAddress'  : get_sheet_value(admin_sheet, 24, 1),
         'municipality'    : get_sheet_value(admin_sheet, 24, 3),
         'province'        : get_sheet_value(admin_sheet, 26, 1),
-        'postal_code'     : get_sheet_value(admin_sheet, 26, 3)
+        'postalCode'     : get_sheet_value(admin_sheet, 26, 3)
     }
 
     co_addr =  {
-        'street_address'  : get_sheet_value(cert_sheet, co_header_idx + 11, 1),
+        'streetAddress'  : get_sheet_value(cert_sheet, co_header_idx + 11, 1),
         'municipality'    : get_sheet_value(cert_sheet, co_header_idx + 11, 4),
         'province'        : get_sheet_value(cert_sheet, co_header_idx + 13, 1),
-        'postal_code'     : get_sheet_value(cert_sheet, co_header_idx + 13, 4)
+        'postalCode'     : get_sheet_value(cert_sheet, co_header_idx + 13, 4)
     }
 
     contacts = {
         "certifying_official_contact":{
-            'first_name'      : get_sheet_value(cert_sheet, co_header_idx + 7, 1),
-            'last_name'       : get_sheet_value(cert_sheet, co_header_idx + 7, 4),
+            'firstName'      : get_sheet_value(cert_sheet, co_header_idx + 7, 1),
+            'lastName'       : get_sheet_value(cert_sheet, co_header_idx + 7, 4),
             'position'        : get_sheet_value(cert_sheet, co_header_idx + 9, 1),
             'email'           : get_sheet_value(cert_sheet, co_header_idx + 9, 4),
             'phone'           : get_sheet_value(cert_sheet, co_header_idx + 9, 6),
             'address'         : co_addr
         },
         "operational_representative_contact":{
-            'first_name'      : admin_sheet.cell_value(18, 1),
-            'last_name'       : admin_sheet.cell_value(18, 3),
+            'firstName'      : admin_sheet.cell_value(18, 1),
+            'lastName'       : admin_sheet.cell_value(18, 3),
             'position'        : admin_sheet.cell_value(20, 1),
             'email'           : admin_sheet.cell_value(20, 3),
             'phone'           : admin_sheet.cell_value(22, 1),
